346a599ce3a79ac04d6591058dd2902624f8ad18
marc hoffman <mh@remobjects.com>
2015-04-01 13:27:09 +0200

    FakeSugar.pas, so we can compile this library for Echoes w/o Sugar dependency

---
+++ Properties/FakeSugar.pas
@@ -0,0 +1,16 @@
+﻿namespace RemObjects.CodeGen4;
+
+interface
+
+type
+  Sugar.Dummy = class;
+  Sugar.IO.Dummy = class;
+  Sugar.Collections.Dummy = class;
+  Sugar.Linq.Dummy = class;
+
+  // These classes exist so that on .NET, CodeGen4 can be build w/o dependency on Sugar, by adding
+  // System.Collections.Generic,System.IO,System.Linq to the project's default uses.
+
+implementation
+
+end.
