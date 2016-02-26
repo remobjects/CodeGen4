f838b787d91b769dfaaa12a21243c2afe9f08c54
marc hoffman <mh@remobjects.com>
2016-02-26 10:23:37 -0400

    Expanded FakeSugar to match what RO had (fixes)

--- Properties/FakeSugar.pas
+++ Properties/FakeSugar.pas
@@ -2,6 +2,7 @@
 
 interface
 
+{$IF ECHOES}
 type
   Sugar.Dummy = class;
   Sugar.IO.Dummy = class;
@@ -13,7 +14,7 @@ type
 
   Sugar.Convert = public static class
   public
-    method ToHexString(Value: Int32; Width: Integer := 0): String;
+    method ToHexString(aValue: Int32; aWidth: Integer := 0): String;
     method ToString(aValue: Byte; aBase: Integer := 10): String;
     method ToString(aValue: Int32; aBase: Integer := 10): String;
     method ToString(aValue: Int64; aBase: Integer := 10): String;
@@ -21,9 +22,11 @@ type
 
 extension method System.String.EqualsIgnoreCase(Value: String): Boolean;assembly;
 extension method System.Xml.XmlNode.ChildCount: Integer;assembly;
+{$ENDIF}
 
 implementation
 
+{$IF ECHOES}
 extension method System.Xml.XmlNode.ChildCount: Integer;
 begin
   exit self.ChildNodes:Count;
@@ -34,30 +37,31 @@ begin
   exit String.Equals(Self, Value, StringComparison.OrdinalIgnoreCase);
 end;
 
-method Sugar.Convert.ToHexString(Value: Int32; Width: Integer): String;
+method Sugar.Convert.ToHexString(aValue: Int32; aWidth: Integer): String;
 begin
-  if Width mod 2 ≠ 0 then Width := Width+1;
+  if aWidth mod 2 ≠ 0 then aWidth := aWidth+1;
 
-  if Width > 16 then Width := 16
-  else if Value > $ffff ffff ffff ff and Width < 16 then Width := 16
-  else if Value > $ffff ffff ffff and Width < 14 then Width := 14
-  else if Value > $ffff ffff ff and Width < 12 then Width := 12
-  else if Value > $ffff ffff and Width < 10 then Width := 10
-  else if Value > $ffff ff and Width < 8 then Width := 8
-  else if Value > $ffff and Width < 6 then Width := 6
-  else if Value > $ff and Width < 2 then Width := 4
-  else Width := 2;
+  if aWidth > 16 then aWidth := 16
+  else if (aValue > $ffff ffff ffff ff) and (aWidth < 16) then aWidth := 16
+  else if (aValue > $ffff ffff ffff) and (aWidth < 14) then aWidth := 14
+  else if (aValue > $ffff ffff ff) and (aWidth < 12) then aWidth := 12
+  else if (aValue > $ffff ffff) and (aWidth < 10) then aWidth := 10
+  else if (aValue > $ffff ff) and (aWidth < 8) then aWidth := 8
+  else if (aValue > $ffff) and (aWidth < 6) then aWidth := 6
+  else if (aValue > $ff) and (aWidth < 2) then aWidth := 4
+  else if (aWidth < 2) then aWidth := 2;
   
   result := '';
-  for i: Integer := Width/2 - 1 downto 0 do begin
-
-    var lCurrentByte := Value shr i mod $ff;
-
-    var Num := lCurrentByte shr 4  and $f;
+  for i: Integer := aWidth/2 - 1 downto 0 do begin
+  
+    var lCurrentByte := aValue shr (i*8) mod $ff;
+  
+    //74540: Bogus nullability wanring, Nougat only
+    var Num := lCurrentByte shr 4 and $f;
     result := result + chr(55 + Num + (((Num - 10) shr 31) and -7));
     Num := lCurrentByte and $f;
     result := result + chr(55 + Num + (((Num - 10) shr 31) and -7));
-    
+      
   end;
 end;
 
@@ -75,5 +79,6 @@ method Sugar.Convert.ToString(aValue: Int64; aBase: Integer := 10): String;
 begin
   result := System.Convert.ToString(aValue, aBase);
 end;
+{$ENDIF}
 
 end.
