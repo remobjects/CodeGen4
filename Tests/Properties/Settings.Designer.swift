b34a0a18e48728a943b41f59ec746444729e158d
marc hoffman <mh@remobjects.com>
2017-08-04 07:01:25 -0400

    Test projects skeleton

---
+++ Tests/Properties/Settings.Designer.swift
@@ -0,0 +1,2 @@
+/* The .Designer.swift file will be automatically generated when the parent file changes.
+   You can also right-click the parent file and select 'Run Custom Tool' to update it now. */
