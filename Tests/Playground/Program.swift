5650da5830b55d33fc50a26e0cabd446dd8825cb
marc hoffman <mh@remobjects.com>
2017-10-30 08:16:23 -0400

    Add test/playground project

---
+++ Tests/Playground/Program.swift
@@ -0,0 +1,17 @@
+﻿import System.Collections.Generic
+import System.Linq
+import System.Text
+
+print("CodeGen4 Playground")
+
+var unit = CGCodeUnit()
+var class1 = CGClassTypeDefinition("ConsoleApplication17.AgeRangeDataSetTableAdapters.SelfReferenceComparer")
+var class2 = CGClassTypeDefinition("TableAdapterManager")
+class1.Members.Add(CGNestedTypeDefinition(class2))
+class2.Members.Add(CGMethodDefinition("foo"))
+unit.Types.Add(class1)
+
+var cg = CGOxygeneCodeGenerator()
+var code = cg.GenerateUnit(unit)
+
+print(code)
