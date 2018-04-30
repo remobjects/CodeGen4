a5b3cd7cd8bf56bf784507c21dc1bfbe2ac9630e
Anton Kasyanov <antonk@remobjects.com>
2018-04-30 14:04:19 +0300

    Binary operators like ≠ ≤ ≥ produce code that cannot be compiled from VS: "(E1) "then" expected, got identifier"

--- CGOxygeneCodeGenerator.swift
+++ CGOxygeneCodeGenerator.swift
@@ -228,9 +228,9 @@
 
 	override func generateBinaryOperator(_ `operator`: CGBinaryOperatorKind) {
 		switch (`operator`) {
-			case .NotEquals: Append("≠")
-			case .LessThanOrEquals: Append("≤")
-			case .GreatThanOrEqual: Append("≥")
+			case .NotEquals: Append("<>")
+			case .LessThanOrEquals: Append("<=")
+			case .GreatThanOrEqual: Append(">=")
 			case .Implies: Append("implies")
 			case .IsNot: Append("is not")
 			case .NotIn: Append("not in")
