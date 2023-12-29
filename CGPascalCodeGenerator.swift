239d24df7b087ebce295470ce935be015ad9d840
Elena Pushkariova <elenap@remobjects.com>
2023-12-29 21:44:26 +0200

    E26853: GTD for interface: property shows "public", method doesnt

--- CGPascalCodeGenerator.swift
+++ CGPascalCodeGenerator.swift
@@ -1829,7 +1829,7 @@ public __abstract class CGPascalCodeGenerator : CGCodeGenerator {
 		}
 		Append(";")
 
-		if isUnified {
+		if isUnified && (property.Visibility != .Public){
 			Append(" ")
 			pascalGenerateMemberVisibilityKeyword(property.Visibility)
 			Append(";")
