0c27a0af37ff90de9286e6b79835b714a577edaa
evgenyk <git@evgenysoft.com>
2024-12-12 18:58:47 +0300

    Swift: add `Type` to reserved words

--- CGSwiftCodeGenerator.swift
+++ CGSwiftCodeGenerator.swift
@@ -13,7 +13,7 @@ public class CGSwiftCodeGenerator : CGCStyleCodeGenerator {
 					"associativity", "autoreleasepool", "break", "case", "catch", "class", "continue", "convenience", "default", "defer", "deinit", "didSet", "do", "dynamicType",
 					"else", "enum", "extension", "fallthrough", "false", "final", "for", "func", "get", "guard", "if", "import", "in", "infix", "init", "inout", "internal", "is",
 					"lazy", "left", "let", "mutating", "nil", "none", "nonmutating", "open", "operator", "optional", "override", "postfix", "precedence", "prefix", "private", "protocol", "public",
-					"repeat", "required", "rethrows", "return", "right", "self", "Self", "set", "static", "strong", "struct", "subscript", "super", "switch", "throw", "throws", "true", "try", "typealias",
+					"repeat", "required", "rethrows", "return", "right", "self", "Self", "set", "static", "strong", "struct", "subscript", "super", "switch", "throw", "throws", "true", "try", "Type", "typealias",
 					"unowned", "var", "weak", "where", "while", "willSet"].ToList() as! List<String>
 
 		softKeywords = ["as"].ToList() as! List<String>
