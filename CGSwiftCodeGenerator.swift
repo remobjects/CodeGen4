d6946f1785efeeec938b3955b56e62e0ef22edf8
evgenyk <git@evgenysoft.com>
2026-03-17 13:29:59 +0300

    Swift: generate assert in `generatePointerDereferenceExpression` method

--- CGSwiftCodeGenerator.swift
+++ CGSwiftCodeGenerator.swift
@@ -526,7 +526,7 @@ public class CGSwiftCodeGenerator : CGCStyleCodeGenerator {
 	}
 
 	override func generatePointerDereferenceExpression(_ expression: CGPointerDereferenceExpression) {
-		//todo
+		assert(false, "generatePointerDereferenceExpression not implemented")
 	}
 
 	override func generateRangeExpression(_ expression: CGRangeExpression) {
